47339b2b06e70f68a79391b04c70647575b27373
Shota Suzuki <s.suzuki@mtl.t.u-tokyo.ac.jp>
2023-10-20 15:51:55 +0900

    add loop out of ap.begincyclecount

--- Processor/Src/Verification/TestCode/C/Apbltcycle/code.c
+++ Processor/Src/Verification/TestCode/C/Apbltcycle/code.c
@@ -9,18 +9,21 @@ int main(int argc, char *argv[])
                   :
                   );
     int a = 0;
-    asm volatile("ap.begincyclecount");
-    for (int i = 0; i < 1000; ++i) {
-        asm goto (
-            "ap.bltcycle %l[ENDING]" 
-            :
-            :
-            :
-            : ENDING);
-        int b = 42;
-        a = a + b;
-        char* ptr = 0x40002000;
-        *ptr = a;
+    for (int j = 0; j < 10; ++j) {
+        a += 1;
+        asm volatile("ap.begincyclecount");
+        for (int i = 0; i < 1000; ++i) {
+            asm goto (
+                "ap.bltcycle %l[ENDING]" 
+                :
+                :
+                :
+                : ENDING);
+            int b = 42;
+            a += b;
+            char* ptr = 0x40002000;
+            *ptr = a;
+        }
     }
 ENDING:
     return 0;
