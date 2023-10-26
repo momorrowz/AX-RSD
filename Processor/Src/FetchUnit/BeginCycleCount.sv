9bc681cba629d28fcb78f61178f13a1240948bbc
Shota Suzuki <s.suzuki@mtl.t.u-tokyo.ac.jp>
2023-10-26 16:52:59 +0900

    update begincycle to flipflop

--- Processor/Src/FetchUnit/BeginCycleCount.sv
+++ Processor/Src/FetchUnit/BeginCycleCount.sv
@@ -10,15 +10,27 @@ module BeginCycleCount #()
     output DataPath begincycle //開始サイクル
 );
 
+logic update;
+
+FlipFlopWE#( DATA_WIDTH, DATA_ZERO ) 
+    CycleReg( 
+        .out( begincycle ), 
+        .in ( cyclecounter ),
+        .we ( update ), 
+        .clk( port.clk ),
+        .rst( port.rst )
+    );
+
 always_comb begin
+    update = FALSE;
     for(int i = 0; i < FETCH_WIDTH; ++i) begin
         if (fetch.bufferHit[i]) begin
-            begincycle = cyclecounter; 
+            update = TRUE;
         end
     end
     for (int i = 0; i < INT_ISSUE_WIDTH; ++i) begin
         if (port.brResult[i].isApBCC && !port.brResult[i].bufHit) begin
-            begincycle = cyclecounter;
+            update = TRUE;
         end
     end
 end
